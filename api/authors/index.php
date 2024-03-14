e6027e9bdcc57c95c7304b98d3f11e9a2426ef9a
Dawsyn <48812637+Dawsyn@users.noreply.github.com>
2024-03-14 09:55:17 -0500

    Update index.php

--- api/authors/index.php
+++ api/authors/index.php
@@ -10,3 +10,23 @@ header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
         exit();
     }
+
+if ($method === 'GET') {
+    // Check if an 'id' parameter is present in the URL
+    if (isset($_GET['id']) && !empty($_GET['id'])) {
+        // If an 'id' is provided, include the script for a single resource
+        include_once 'read_single.php';
+    } else {
+        // If no 'id' is provided, include the script to read all resources
+        include_once 'read.php';
+    }
+}
+
+if ($method === 'POST')
+  include_once 'create.php';
+
+if ($method === 'PUT') 
+  include_once 'update.php';
+
+if ($method === 'DELETE')
+  include_once 'delete.php';  
